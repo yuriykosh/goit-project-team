44bdc5e6063dbb9dcb11fc9ec041fbbae9b0cbb8
olabudnik <olyabudnik98@gmail.com>
2022-11-10 19:11:56 +0200

    Розмітка і сss

---
+++ src/index.js
@@ -0,0 +1,16 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:0,
+    nav:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
