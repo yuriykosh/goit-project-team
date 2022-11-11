017c4cfb6669c3657fc853cb3b8bc521d60cb51c
olabudnik <olyabudnik98@gmail.com>
2022-11-11 22:46:44 +0200

    reviews no js

---
+++ src/js/index.js
@@ -0,0 +1,10 @@
+var owl = $('.owl-carousel');
+owl.owlCarousel({
+    items:1, 
+  // items change number for slider display on desktop
+  
+    loop:true,
+    autoplay:true,
+    autoplayTimeout:3000,
+    autoplayHoverPause:true
+});
