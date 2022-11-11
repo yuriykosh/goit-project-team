e599ba4f1283b041d8309cd4619b4cf26d2d234b
yuriykosh <yuriy.koshyk@gmail.com>
2022-11-11 22:09:13 +0000

    Deploying to gh-pages from @ yuriykosh/goit-project-team@828dfe44eb50b631fa0f5c8108d4093c818ce9bb 🚀

---
+++ index.0ad27011.js
@@ -0,0 +1,2 @@
+(()=>{const o={openModalBtn:document.querySelector("[data-modal-products-open]"),closeModalBtn:document.querySelector("[data-modal-products-close]"),modal:document.querySelector("[data-modal-products]")};function e(){o.modal.classList.toggle("is-hidden")}o.openModalBtn.addEventListener("click",e),o.closeModalBtn.addEventListener("click",e)})();
+//# sourceMappingURL=index.0ad27011.js.map
