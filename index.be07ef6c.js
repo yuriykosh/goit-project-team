42b242a21bbb5542a1bc9e9b561310712ea76121
yuriykosh <yuriy.koshyk@gmail.com>
2022-11-11 22:06:26 +0000

    Deploying to gh-pages from @ yuriykosh/goit-project-team@2b461148db1fda7ca837d0d11f8cf3f6d1fa516e 🚀

---
+++ index.be07ef6c.js
@@ -0,0 +1,2 @@
+$(document).ready((function(){$(".slider").slick({slidesToShow:1,slidesToScroll:1,infinite:!0,centerMode:!0,centerPadding:"1px"}),$(".slick-prev").text("❯"),$(".slick-next").text("❯")}));
+//# sourceMappingURL=index.be07ef6c.js.map
