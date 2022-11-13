0f8cf1091b3addf229f6b0de0f8752ef395a811b
Yuriy Koshyk <yuriy.koshyk@gmail.com>
2022-11-13 00:41:09 -0500

    Fixed bugs

--- src/js/modal-products.js
+++ src/js/modal-products.js
@@ -1,8 +1,34 @@
 (() => {
     const refs = {
-        openModalBtn: document.querySelector("[data-modal-products-open]"),
-        closeModalBtn: document.querySelector("[data-modal-products-close]"),
-        modal: document.querySelector("[data-modal-products]"),
+        openModalBtn: document.querySelector("[data-modal-products1-open]"),
+        closeModalBtn: document.querySelector("[data-modal-products1-close]"),
+        modal: document.querySelector("[data-modal-products1]"),
+    };
+    refs.openModalBtn.addEventListener("click", toggleModal);
+    refs.closeModalBtn.addEventListener("click", toggleModal);
+    function toggleModal() {
+        refs.modal.classList.toggle("is-hidden");
+    }
+})();
+
+(() => {
+    const refs = {
+        openModalBtn: document.querySelector("[data-modal-products2-open]"),
+        closeModalBtn: document.querySelector("[data-modal-products2-close]"),
+        modal: document.querySelector("[data-modal-products2]"),
+    };
+    refs.openModalBtn.addEventListener("click", toggleModal);
+    refs.closeModalBtn.addEventListener("click", toggleModal);
+    function toggleModal() {
+        refs.modal.classList.toggle("is-hidden");
+    }
+})();
+
+(() => {
+    const refs = {
+        openModalBtn: document.querySelector("[data-modal-products3-open]"),
+        closeModalBtn: document.querySelector("[data-modal-products3-close]"),
+        modal: document.querySelector("[data-modal-products3]"),
     };
     refs.openModalBtn.addEventListener("click", toggleModal);
     refs.closeModalBtn.addEventListener("click", toggleModal);
