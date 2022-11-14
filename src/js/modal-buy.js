d8f18547b6ef37f7570af52fc52ca7ea303225f1
Yuriy Koshyk <yuriy.koshyk@gmail.com>
2022-11-13 22:18:26 -0500

    Bugs fixed

--- src/js/modal-buy.js
+++ src/js/modal-buy.js
@@ -10,3 +10,16 @@
     refs.modal.classList.toggle('is-hidden');
   }
 })();
+
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector('[data-modal-buy-mobile-open]'),
+    closeModalBtn: document.querySelector('[data-modal-buy-mobile-close]'),
+    modal: document.querySelector('[data-modal-buy-mobile]'),
+  };
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+  function toggleModal() {
+    refs.modal.classList.toggle('is-hidden');
+  }
+})();
